5d79218b710968c399e8a6109250897253d9caf4
rock10578 <shreyanshpatwa26@gmail.com>
2025-09-14 20:52:29 +0530

    Product service file  added

--- src/app/pages/admin/products/products.ts
+++ src/app/pages/admin/products/products.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
 export class Products {
-
+  isSidePanelVisible: boolean=false;
+  productObj: any = {
+    "productId": 0,
+    "productSku": "",
+    "productName": "",
+    "productPrice": 0,
+    "productShortName": "",
+    "productDescription": "",
+    "createdDate": new Date(),
+    "deliveryTimeSpan": new Date(),
+    "categoryId": 2147483647,
+    "productImageUrl": "",
+    "uerId": 0
+  }
+  openSidePanel(){
+    this.isSidePanelVisible = true;
+  }
+  closeSidePanel(){
+    this.isSidePanelVisible = false;
+  }
 }
